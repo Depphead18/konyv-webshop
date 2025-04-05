2e770c944104632878e38cca65ce28c432971cd7
h258208 <h258208@stud.u-szeged.hu>
2025-04-05 18:00:17 +0200

    menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component'
+import { RegistrateComponent } from './pages/registrate/registrate.component';
+import { KonyvlistaComponent } from './pages/konyvlista/konyvlista.component';
+import { KonyvreszletekComponent } from './pages/konyvreszletek/konyvreszletek.component';
+import { LoginComponent } from './pages/login/login.component';
+import { KosarComponent } from './pages/kosar/kosar.component';
+import { TBRComponent } from "./pages/tasks/tbr.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Statikus elérési útvonalak
+    { path: 'home', title: 'Főoldal', component: HomeComponent },
+
+    { path: 'tbr', title: 'TBR Lista', component: TBRComponent },
+
+    { path: 'profile', title: 'Profil', component: ProfileComponent, },
+
+    { path: 'registrate', title: 'Regisztráció', component: RegistrateComponent },
+
+    { path: 'konyvlista', title: 'Könyveink', component: KonyvlistaComponent },
+
+    { path: 'konyvreszletek', title: 'Részletek', component: KonyvreszletekComponent },
+
+    { path: 'login', title: 'Bejelentkezés', component: LoginComponent },
+
+    { path: 'kosar', title: 'Kosár', component: KosarComponent },
+
+    { path: '**', title: 'Page-Not-Found', component: PageNotFoundComponent }, //ha egyik útvonal sem egyezik
+
+    // Paraméterezett útvonalak
+    // { path: 'task-edit/:id', component: TaskEditComponent },
+
+    // Üres elérési út - alapértelmezett útvonal
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    
+];
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,27 +1,31 @@
-import { Component, AfterViewInit, OnInit, Output, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent implements OnInit, AfterViewInit {
+export class MenuComponent implements OnInit, AfterViewInit{
+
   @Output() selectedPage: EventEmitter<string> = new EventEmitter();
-  
+
   constructor(){
-    console.log('c');
+    console.log("construtor called");
   }
 
-  ngOnInit(): void{
-    console.log("ng");
+  ngOnInit(): void {
+    console.log("ngOnInit called");
   }
 
   ngAfterViewInit(): void {
-    console.log('');
+    console.log("ngAfterViewInit called");
   }
 
-  menuSwitch(pageValue: string){
+  menuSwitch(pageValue: string) {
     this.selectedPage.emit(pageValue);
   }
+
 }
